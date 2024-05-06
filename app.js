2c4f795110a577c184ac824a4be18ca1f94e19b2
justino2m <justin.viola@o2m.systems>
2024-05-07 01:42:51 +0200

    cors

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control'
     );
     res.header('Access-Control-Allow-Credentials', 'true');
     if ('OPTIONS' === req.method) {
